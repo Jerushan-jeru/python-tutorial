44889fba166dedca137857f1fd5ea3369d7f7fae
jerushan_jeru <srijeru98@gmail.com>
2022-02-20 20:12:57 +0530

    new python

---
+++ introduction-to-python-for-beginners.py
@@ -0,0 +1,19 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ( ' Hello world ' )
+ Hello world 
+Greeting = ' Hello world '
+print (Greeting)
+ Hello world 
+name=
+SyntaxError: invalid syntax
+
+name = 'Jerushan'
+print (name)
+Jerushan
+print ( ' Hello ',name, ' Well come this class ')
+ Hello  Jerushan  Well come this class 
+msg1= 'Hello'
+msg2= ' Well come to python '
+print (msg1+name+msg2)
+HelloJerushan Well come to python 
