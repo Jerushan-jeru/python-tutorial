5bf61aee9bce55825842faa0c2dd1e474013e985
jerushan_jeru <srijeru98@gmail.com>
2022-02-20 21:49:21 +0530

    update day 2

---
+++ understanding-and-using-python-lists-and-arrays.py
@@ -0,0 +1,75 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Grade1=97
+Grade2=88
+Grede3=65
+print (Grade2)
+88
+Grades= [97,87,54,74]
+print (Grades)
+[97, 87, 54, 74]
+print (Grades[2])
+54
+print(Grades [1])
+87
+x = Grades [2] + Grades [0]
+print (x)
+151
+print (Grades)
+[97, 87, 54, 74]
+Grades [0]
+97
+picture= [[1,2,3,4,5] , [6,7,8,9,10],[11,12,13,14]]
+print picture
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print (picture)
+[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14]]
+print [0]
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    print [0]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+print (picture[1])
+[6, 7, 8, 9, 10]
+print (picture[1][2])
+8
+fruits = ['apple','banana','orange']
+print (fruits)
+['apple', 'banana', 'orange']
+print (friuts [1])
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print (friuts [1])
+NameError: name 'friuts' is not defined. Did you mean: 'fruits'?
+print (fruits[1])
+banana
+grades = [21,34,34,53,'banana',43,23]
+print (grades[3])
+53
+print (grades[4])
+banana
+x=43
+y=34
+z=87
+a=54
+d=54
+grades=[x,y,z,a,d]
+print(grades)
+[43, 34, 87, 54, 54]
+fruits = ['apple','banana','orange']
+print (fruits [1])
+banana
+print (fruits[1][3])
+a
+grades [2]=55
+x=[]
+print (x)
+[]
+x.append(43)
+print (x)
+[43]
+x.append (54)
+print (x)
+[43, 54]
+print (x[1])
+54
