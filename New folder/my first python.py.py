791214d64e833e2c496dd3c8c8012086c65529f6
jerushan_jeru <srijeru98@gmail.com>
2022-02-23 23:27:50 +0530

    update

---
+++ New folder/my first python.py.py
@@ -0,0 +1,2 @@
+name=input ("Please Enter your Neame : ")
+print ('Wellcome to ', name ,'our python coures')
---
+++ getting-user-input-from-keybord.py
@@ -0,0 +1,30 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=7
+y=3
+z=x+y
+print (z)
+10
+
+= RESTART: C:/Users/DreamSpace/Desktop/python-tutorial/New folder/my fiast python.py
+Hello world
+
+= RESTART: C:/Users/DreamSpace/Desktop/python-tutorial/New folder/my fiast python.py
+7 + 2 = 9
+
+= RESTART: C:/Users/DreamSpace/Desktop/python-tutorial/New folder/my fiast python.py
+7 + 2 = 9
+Please input the namber4
+Your namber is 4
+
+= RESTART: C:/Users/DreamSpace/Desktop/python-tutorial/New folder/my fiast python.py
+7 + 2 = 9
+Please input the namber ; 4
+Your namber is 4
+
+= RESTART: C:/Users/DreamSpace/Desktop/python-tutorial/New folder/my fiast python.py
+7 + 2 = 9
+Please input the namber ; 5
+Your namber is 5
+Please Enter Your Name Jerushan
+Hello Jerushan Welcome to the python land
